e038760c911eac800d8c6e31ad111cfedc5d7732
usuario <usuario@10.22.143.64>
2016-10-13 11:04:40 +0100

    Representacion gráfica de un histograma2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -6,7 +6,15 @@ public class Kata3 {
 
    
     public static void main(String[] args) {
-       HistogramDisplay histo = new HistogramDisplay();
-       histo.execute();
+       Histogram<String> histogram = new Histogram();
+       histogram.increment("gmail.com");
+       histogram.increment("gmail.com");
+       histogram.increment("ulppgc.es");
+       histogram.increment("ulppgc.es");
+       histogram.increment("ull.es");
+       histogram.increment("ull.es");
+       histogram.increment("hotmail.com");
+       
+       new HistogramDisplay(histogram).execute();
     }
 }
